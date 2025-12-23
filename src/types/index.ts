b46b7a5bebcdf43ba5bb0f246ae36006fdafd025
Jibles <premiumjibles@gmail.com>
2025-12-24 05:29:02 +0530

    feat: migrate to daily granularity and add daily chart

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,18 @@
+export type DailyRevenue = {
+  totalUsd: number
+  byService: Record<string, number>
+}
+
 export type AffiliateRevenueResponse = {
   totalUsd: number
   byService: Record<string, number>
+  byDate: Record<string, DailyRevenue>
   failedProviders: string[]
 }
 
 export type DateRange = {
-  startTimestamp: number
-  endTimestamp: number
+  startDate: string
+  endDate: string
 }
 
 export type ServiceRevenue = {
